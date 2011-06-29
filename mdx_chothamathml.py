6a43ae577267a5bc95ac6f1ae24d2d64a26009b9
kghose <kghose@9bf5578e-1440-42ad-ab19-faf6e26ab722>
2011-06-29 22:02:07 +0000

    latex markdown filter debugged and working

--- mdx_chothamathml.py
+++ mdx_chothamathml.py
@@ -1,34 +1,47 @@
 """
-A filter to let asciimath and latexmath stuff emerge unscathed through markdown
-Based on the markdown extension documents and
-python-ascii math
+A filter to let math expressions in latex emerge unscathed through markdown
+Based on the markdown extension documents and python-ascii math at
 https://github.com/favalex/python-asciimathml/blob/e4c808d096f3ae270f5305b944a66c31a37dc853/mdx_asciimathml.py
 
-A bit more sophisticated than the regexp search and hide tecqnique I used in RRiki
+The filename has to start with 'mdx_' and the rest of the name is what must be
+put in the 'extensions' list when markdown is invoked
+
+A bit more sophisticated than the regexp search and hide technique I used in RRiki
 """
 
 import re, markdown
 
-class ASCIIMathMLExtension(markdown.Extension):
-    def __init__(self, configs):
-        pass
+regexp = r"""
+^(.*) #Markdown regex needs to start with this
+\$(.*?)\$ #Non greedy match anything between $ sign 
+(.*)$ #Needs to end with this
+"""
 
-    def extendMarkdown(self, md, md_globals):
-        self.md = md
+class escapeLaTeXExtension(markdown.Extension):
+  def __init__(self, configs):
+    pass
+  
+  def extendMarkdown(self, md, md_globals):
+    self.md = md
+    """The name of this extension is escapeLaTeX and it is inserted at the
+    head of all the markdown operations (_begin)."""
+    md.inlinePatterns.add('escapeLaTeX', escapeLaTeXPattern(md), '_begin')
+      
+  def reset(self):
+    pass
 
-        RE = re.compile(r'^(.*)\$\$([^\$]*)\$\$(.*)$', re.M) # $$ a $$
-
-        md.inlinePatterns.add('', ASCIIMathMLPattern(RE), '_begin')
-
-    def reset(self):
-        pass
-
-class ASCIIMathMLPattern(markdown.inlinepatterns.Pattern):
-
-    def handleMatch(self, m):
-        el = markdown.etree.Element('span')
-        el.text = "$" + m.group(2) + "$"
-        return el
+class escapeLaTeXPattern(markdown.inlinepatterns.Pattern):
+  def getCompiledRegExp(self):
+    """The regexp is meant to run multiline mode and we use VERBOSE because we
+    commented the regexp so nicely."""
+    return re.compile(regexp, re.M | re.VERBOSE)
+    
+  def handleMatch(self, m):
+    """Markdown regexp processes the whole block, and so out regexp match is
+    group(2)."""
+    el = markdown.etree.Element('span')
+    el.text = markdown.AtomicString("$" + m.group(2) + "$")
+    return el
 
 def makeExtension(configs=None):
-    return ASCIIMathMLExtension(configs=configs)
+    return escapeLaTeXExtension(configs=configs)
