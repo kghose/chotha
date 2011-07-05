6fae457f89f782180c086862b761b1f4c0bc866e
kghose <kghose@9bf5578e-1440-42ad-ab19-faf6e26ab722>
2011-07-05 17:25:06 +0000

    documentation

--- mdx_chothamathml.py
+++ mdx_chothamathml.py
@@ -4,7 +4,9 @@ Based on the markdown extension documents and python-ascii math at
 https://github.com/favalex/python-asciimathml/blob/e4c808d096f3ae270f5305b944a66c31a37dc853/mdx_asciimathml.py
 
 The filename has to start with 'mdx_' and the rest of the name is what must be
-put in the 'extensions' list when markdown is invoked
+put in the 'extensions' list when markdown is invoked i.e.
+
+  md = markdown.Markdown(extensions=['chothamathml']).convert
 
 A bit more sophisticated than the regexp search and hide technique I used in RRiki
 """
@@ -37,8 +39,8 @@ class escapeLaTeXPattern(markdown.inlinepatterns.Pattern):
     return re.compile(regexp, re.M | re.VERBOSE)
     
   def handleMatch(self, m):
-    """Markdown regexp processes the whole block, and so out regexp match is
-    group(2)."""
+    """Markdown regexp processes the whole block, and so our regexp match is
+    group(2) with group(1) and group(3) being the leading and trailing."""
     el = markdown.etree.Element('span')
     el.text = markdown.AtomicString("`" + m.group(2) + "`")
     return el
